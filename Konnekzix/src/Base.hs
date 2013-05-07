c054f75804a0629aa41699e43f62d3b1b9384fed
JakeBr <jake.bruenker@gmail.com>
2013-05-08 00:02:03 +0200

    refactored and debugged

--- Konnekzix/src/Base.hs
+++ Konnekzix/src/Base.hs
@@ -17,18 +17,24 @@ module Base where
 
 import Data.Array
 import Data.Maybe
-import Data.List.Split
-import Data.List
 
 -- | 'Board' is the type for storing the current data of a game. It contains
 -- an array, which contains an element for every intersection on the board.
 -- Each element may contain a stone and contains some information to see if
 -- the stone is part of a row of multiple stones, horizontally, vertically,
 -- or diagonally. To construct a board, use 'empty'.
-newtype Board = Board (Array Coords (Maybe Stone, Array Int Row)) deriving Show
+newtype Board = Board (Array Coords (Maybe Stone, Array Direction Row))
+              deriving Show
 
 -- | On each intersection of a board, there may be a Stone.
 newtype Stone = Stone Color deriving (Show, Eq)
+
+-- | The directions of the 'Row's
+data Direction = West
+               | North
+               | NorthWest
+               | NorthEast
+               deriving (Eq, Ord, Enum, Ix, Show)
 
 -- | Each player and their stones have a 'Color'.
 data Color = Black
@@ -40,7 +46,7 @@ type Coords = (Int,Int)
 
 -- | A 'Row' value will tell you how many stones of the same color as this
 -- one are placed in front of it. If there is no Stone, it will tell you
--- how many empty intersections are in front of it. (\"In front\" actually
+-- how many empty intersections are in front of it. (\"in front\" actually
 -- means either above it, to the left, to the top-left or to the top-right.)
 type Row = Int
 
@@ -48,12 +54,17 @@ type Row = Int
 type Size = Int
 
 -- | The 'empty' function lets you construct a 'Board'. You need to give it
--- size which is greater than six.
+-- size which is greater than five.
 empty :: Size -> Either String Board
 empty s = if   s < 6 then Left "The size must be greater than five!"
-          else Right . Board . fmap ((Nothing,) . listArray (1,4))
+          else Right . Board . fmap ((Nothing,) . listArray (West,NorthEast))
             . listArray ((1,1),(s,s)) $
-            [ [x,y,min x y, min (s - x + 1) y] | y <- [1..s], x <- [1..s] ]
+            [ [x,y,min x y, min (s - x + 1) y] | x <- [1..s], y <- [1..s] ]
+
+-- | 'isOutOfBoundsOf' checks, whether the given Coordinates are valid for
+-- a given Board
+isOutOfBoundsOf :: Coords -> Board -> Bool
+isOutOfBoundsOf coords (Board arr) = not $ inRange (bounds arr) coords
 
 -- | With 'placeStone', you can place a stone on a board, who'd a thunk? If
 -- someone has won because of that stone, you'll get a boolean truth in
@@ -61,109 +72,111 @@ empty s = if   s < 6 then Left "The size must be greater than five!"
 -- or even outside of the board, it will give you a String. It will also
 -- update the 'Row's.
 placeStone :: Board -> Stone -> Coords -> Either String (Bool, Board)
-placeStone (Board arr) stone coords@(x,y)
-  | not $ inRange bs coords =
-    Left "Coordinates are out of bounds!"
+placeStone board@(Board arr) stone coords
+  |  coords `isOutOfBoundsOf` board =
+      Left "Coordinates are out of Bounds!"
   | isJust . fst $ arr ! coords =
-    Left "Stones can only be placed on empty intersections"
-  | otherwise = Right
-                (won, Board fixSucs)
-  where (wonPred,getPredRows) =
-          let rows = map getRow [1..4]
-          in  (any id $ map fst rows,
-              listArray (1,4) . map snd $ rows)
-        bs = bounds arr
-        won = wonPred || wonSucs
-        getRow d = let predPos = arr ! cs'
-                       row     = (snd predPos ! d) + 1
-                       cs' = cs (-1) d
-                       cs'notInRange = fst cs' < 1 || snd cs' < 1
-                   in  if   cs'notInRange || fst predPos /= Just stone
-                       then (False, 1)
-                       else (row >= 6,row)
-        cs n d
-          | d == 1    = (x + n, y    )
-          | d == 2    = (x    , y + n)
-          | d == 3    = (x + n, y + n)
-          | otherwise = (x - n, y + n)
-        (wonSucs,fixSucs) = let fixed = map (fix 2 .
-                                        (\d -> (d,map (`cs` d) [1..])))
-                                        [1..4]
-                            in  (any id $ map fst fixed
-                                ,arr' // concatMap snd fixed)
-        arr' = arr // [(coords,(Just stone,getPredRows))]
-        fix :: Int -> (Int, [Coords]) ->
-               (Bool,[(Coords, (Maybe Stone, Array Int Row))])
-        fix n (d, l@(xy:xys))
-          | not $ inRange bs xy       = (False,[])
-          | isNothing $ fst nextPos   = (False,fixNothings 1 l)
-          | fst nextPos /= Just stone = (False,[])
-          | otherwise                 = (n >= 5 || fst nextFix,
-                                        snd nextFix ++
-                                        [(xy,mapSnd (//[(d,n)]) nextPos)])
-          where nextFix = fix (n + 1) (d,xys)
-                nextPos = arr ! xy
-                mapSnd f (p,q) = (p,f q)
-                fixNothings :: Int -> [Coords] ->
-                                [(Coords, (Maybe Stone, Array Int Row))]
-                fixNothings n' (xy':xy's)
-                  | not $ inRange bs xy'  = []
-                  | isJust $ fst nextPosN = []
-                  | otherwise             = nextFixN ++
-                                            [(xy, mapSnd (//[(d,n')])
-                                            nextPosN)]
-                  where nextPosN = arr ! xy'
-                        nextFixN = fixNothings (n' + 1) xy's
-                fixNothings _ _ = []
-        fix _ _ = (False,[])
--- Geeze, that's kind of a long function. Gonna fix that while refactoring
--- the code.
+      Left "Stones can only be placed on empty intersections"
+  | otherwise =
+      Right . fixRows coords . replace stone coords $ board
+
+-- | 'replace' let's you replace a stone without error handling. (the program
+-- won't crash. If something is invalid, it will just return the original
+-- 'Board'.) It will also not fix the 'Row's.
+replace :: Stone -> Coords -> Board -> Board
+replace stone coords board@(Board arr) =
+  if   coords `isOutOfBoundsOf` board
+  then board
+  else Board $ arr // [(coords,(Just stone, snd old))]
+    where old = arr ! coords
+
+-- | 'fixRows' will fix every 'Row' of a given 'Board'.
+fixRows :: Coords -> Board -> (Bool, Board)
+fixRows coords board =
+  (any fst $ concat fixes, foldr1 (.) (map (flip (//>) . map snd) fixes) board)
+  where fixes = map (fix board coords ) $
+          zip3
+            dirs
+            (map prevStoneIn dirs)
+            (map prevRowsIn dirs)
+        dirs = [West .. NorthEast]
+        prevStoneIn dir = getStone board $ goTo coords dir 1
+        prevRowsIn dir  = getRow board (goTo coords dir 1) dir
+
+-- | 'fix' fixes a single continuous 'Row'. It is basically a helper
+-- function for 'fixRows'.
+fix :: Board -> Coords -> (Direction, Maybe Stone, Row) ->
+  [(Bool, (Coords, Direction, Row))]
+fix board coords (dir, prevStone, prevRow)
+  | coords `isOutOfBoundsOf` board = []
+  | isFine                         = []
+  | prevStone /= thisStone         =
+      (False, (coords, dir, 1)) : fix board newCoords (dir, thisStone, 1)
+  | otherwise                      =
+      (prevRow >= 5 && isJust thisStone, (coords, dir, prevRow + 1)) :
+        fix board newCoords (dir, thisStone, prevRow + 1)
+  where isFine = (prevStone == thisStone && prevRow == thisRow - 1) ||
+                 (prevStone /= thisStone && thisRow == 1)
+        thisRow   = getRow   board coords dir
+        thisStone = getStone board coords
+        newCoords = goTo coords dir (-1)
+
+-- | 'goTo' will give you coordinates for going a certain number of
+-- intersections in a certain direction from the initial coordinates.
+goTo :: Coords -> Direction -> Int -> Coords
+goTo (x,y) dir amount = case dir of
+  West      -> (x - amount, y         )
+  North     -> (x         , y - amount)
+  NorthWest -> (x - amount, y - amount)
+  NorthEast -> (x + amount, y - amount)
+
+-- | '//>' is a variation of 'Data.Array.//' for specific usage with 'Row's.
+(//>) :: Board -> [(Coords, Direction, Row)] -> Board
+(//>) (Board arr) news = Board $ arr // map rephrase news
+  where rephrase (coords,dir,row) =
+          let old = arr ! coords
+          in  (coords, (fst old, snd old // [(dir,row)]))
+
+-- | 'getRow' returns the Row in a certain direction at given coordinates
+getRow :: Board -> Coords -> Direction -> Row
+getRow (Board arr) coords dir = snd (arr ! coords) ! dir
+
+-- | 'getStone' looks at a intersection and returns Just the Stone or
+-- Nothing, respectively.
+getStone :: Board -> Coords -> Maybe Stone
+getStone (Board arr) coords = fst (arr ! coords)
 
 --------------------------------------------------------------------------------
--- DEBUGGING STUFF FOR GHCI; TEMPORARY?
+-- debugging stuff for ghci; temporary?
 
-prettyDebug :: Board -> String
-prettyDebug (Board arr) = firstLn ++ concatPieces (fmap (toPiece 1) arr) ++ "|"
-  where firstLn      = concat (replicate maxX " _____") ++ "\n"
-        toPiece n e  = let isNewLn  = n > maxX
-                           endBar   = if isNewLn then "|"  else ""
-                           newLn    = if isNewLn then "\n" else ""
-                           sndLn    = fmap cutoff [d 3,0,d 2,0,d 4]
-                           thrdLn   = cutoff (d 1) : "____"
-                           cutoff x
-                             | x == 0    = ' '
-                             | otherwise = last . show $ x
-                           d a      = snd e ! a
-                       in  [ "|" ++ stone e ++ endBar ++ newLn
-                           , "|" ++ sndLn   ++ endBar ++ newLn
-                           , "|" ++ thrdLn  ++ endBar ++ newLn]
-        maxX         = snd . snd . bounds $ arr
-        concatPieces a = let go ((l1,l2,l3):ls) = l1 ++ l2 ++ l3 ++ go ls
-                             go [] = []
-                         in  addNewLns . go $ zip3 (line a 1) (line a 2) (line a 3)
-        line a n     = chunksOf (maxX * 6) $ concat (fmap (!! (n - 1)) (elems a))
-        addNewLns s  = let go wasB (x:xs)
-                             | wasB = if x == '|' then go True xs else x : go False xs
-                             | otherwise = if x == '|' then x : go True xs
-                                                       else x : go False xs
-                           go _ [] = []
-                       in  go False . intercalate "|\n" $ chunksOf (maxX * 6) s
-        stone t      = case fst t of
-                         Nothing            -> "Nothn"
-                         Just (Stone Black) -> "Black"
-                         Just (Stone White) -> "White"
+prettyDebug :: Board -> String -- Will only work correctly for boards up to 9x9
+prettyDebug (Board arr) = firstLn ++ '\n' : concatMap prettyRow [1..maxX]
+  where
+    (_,(maxX,_)) = bounds arr
+    prettyRow y  = let fs = map line ([1..3] :: [Int])
+                   in  concat [ concatMap (`f` y) [1..maxX] ++ "|\n" | f <- fs ]
+    line n x y
+      | n == 1    = '|' : showStone (fst intersection)
+      | n == 2    = concat ["|",row NorthWest," ",row North," ",row NorthEast]
+      | n == 3    = '|' : row West ++ "____"
+      | otherwise = "|Error"
+      where intersection = arr ! (x,y)
+            row m        = show $ snd intersection ! m
+    firstLn      = concat (replicate maxX " _____")
+    showStone (Just (Stone col)) = show col
+    showStone _                  = "Nothn"
 
 u :: Board -> IO ()
 u = putStrLn . prettyDebug
 
-m :: Board -> Stone -> Coords -> Board
-m x s c = let (Right (_,b1)) = placeStone x s c in b1
+p :: Board -> Stone -> Coords -> Board
+p x s c = let (Right (_,b1)) = placeStone x s c in b1
 
 b :: Stone
 b = Stone Black
 
 w :: Stone
-w = Stone Black
+w = Stone White
 
 b0 :: Board
-b0 = let (Right b_) = empty 7 in b_
+b0 = let (Right b') = empty 7 in b'
